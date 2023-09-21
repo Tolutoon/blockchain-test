3328bbf476fd61f980545539ae49e5f5f329a3fc
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-09-21 06:27:53 +0100

    in progress

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Welcome, Services, Footer, Transactions } from './components';
+import './App.css'
+
+function App() {
+  return (
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+      <Navbar/>
+      <Welcome/>
+      </div>
+      <Services/>
+      <Transactions />
+      <Footer/>
+    </div>
+
+  )
+}
+
+export default App
